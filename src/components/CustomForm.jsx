621dd8fa043c453abdc94ad9c52ba3163c1a0937
FarelDanendra <147696558+FarelDanendra@users.noreply.github.com>
2025-06-03 20:22:58 +0700

    mengubah nama barang

--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -57,7 +57,7 @@ const CustomForm = ({ addItem }) => {
           required
           placeholder="Nama Barang"
         />
-        <label htmlFor="nama" className="label">Nama Barang</label>
+        <label htmlFor="nama" className="label">Nama Saya</label>
       </div>
       <div className="wrapper">
         <input
