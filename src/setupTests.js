7fa53593df7ed155f012828bb50922c6db5e5117
Ahdaf <mahdaf8@gmail.com>
2025-06-09 20:06:37 +0700

    update

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,19 @@
 import '@testing-library/jest-dom';
 
+// Mock window.matchMedia untuk environment test
+if (typeof window !== "undefined" && !window.matchMedia) {
+  window.matchMedia = function () {
+    return {
+      matches: false,
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      addListener: () => {},
+      removeListener: () => {},
+      dispatchEvent: () => false,
+    };
+  };
+}
+
 // Mock TextEncoder
 global.TextEncoder = require('util').TextEncoder;
 global.TextDecoder = require('util').TextDecoder;
