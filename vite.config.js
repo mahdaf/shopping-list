c5d59ff2dd8c29b3d9886079c8fc206fc6d86b09
Ahdaf <mahdaf8@gmail.com>
2025-06-10 23:26:50 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          firebase: ['firebase']
-        }
-      }
-    }
-  }
+  plugins: [react()]
 })
